30668586bc41b9cba8e1fe762bf9a64811518a70
Mouskyy17 <ismoubarak17@gmail.com>
2025-02-15 15:41:17 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def load_data():
     'Duels aeriens gagnes', 'Courses progressives', 'Ballons recuperes',
     'Passes progressives', 'Dribbles reussis', 'Passes cles',
     'Tirs cadres', 'Touches de balle surface offensive', 'Degagements',
-    'Fautes commises', 'Passes longues reussies', 'Pourcentage passes reussies'
+    'Fautes commises', 'Passes longues reussies', 'Pourcentage de passes reussies'
 ]
     for stat in stats:
         df[f'{stat} par 90'] = per90(df[stat], df['Minutes jouees'])
