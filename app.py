8c1be5e3376210560374131a80aa8882c6bf724f
Mouskyy17 <ismoubarak17@gmail.com>
2025-03-04 19:05:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ def main():
             polar=dict(
                 radialaxis=dict(
                     visible=True,
-                    range=[0, 100],
+                    range=[0, 10],
                     tickfont=dict(size=8)
             )),
             legend=dict(
