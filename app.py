fb3203db5cea65353e712a4c4dfe02e11614ad00
Mouskyy17 <ismoubarak17@gmail.com>
2025-03-29 21:39:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -344,9 +344,9 @@ def main():
 
         # Affichage du radar
         st.markdown("### 📊 Profil comparé (par 90 minutes)")
-        st.plotly_chart(fig)
+        st.plotly_chart(fig, use_container_width=True)
         # Ajustement de la mise en page
-        plt.tight_layout(pad=3.0)
+        plt.tight_layout()
 
         # Tableau comparatif
         st.markdown("### 📊 Comparaison détaillée")
