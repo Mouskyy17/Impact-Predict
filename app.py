3c2e062d773ef40c12707b028fda55b36d6af4b8
Mouskyy17 <ismoubarak17@gmail.com>
2025-03-31 14:23:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -346,10 +346,21 @@ def main():
             compare=True
         )
 
-        
+        # Ajout de la légende personnalisée
+        labels = [player1['Joueur'], player2['Joueur']]
+        colors = ['#9B3647', '#3282b8']
+        patches = [plt.Rectangle((0,0),1,1, color=colors[i], ec="black") for i in range(2)]
+        ax.legend(
+            handles=patches,
+            labels=labels,
+            loc='upper right',
+            bbox_to_anchor=(1.25, 1),  # Ajustez ces valeurs au besoin
+            fontsize=8,
+            frameon=False
+        )
 
         # Ajustement du layout et affichage
-        fig.set_size_inches(8, 7)
+        fig.set_size_inches(8, 8)
         plt.tight_layout(pad=2.0)
         st.pyplot(fig)
        
